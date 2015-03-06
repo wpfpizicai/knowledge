df69050b1794329e307c0d821a89a1f6014bfd65
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-03-06 22:28:51 +0800

    yes

--- App/Conf/config.js
+++ App/Conf/config.js
@@ -4,8 +4,8 @@ module.exports = {
   db_type: 'mysql', // 数据库类型
   db_host: '127.0.0.1', // 服务器地址
   db_port: '', // 端口
-  db_name: '', // 数据库名
-  db_user: 'root', // 用户名
-  db_pwd: '', // 密码
-  db_prefix: 'think_', // 数据库表前缀
+  db_name: 'nodejs', // 数据库名
+  db_user: 'nodejs', // 用户名
+  db_pwd: 'Wpf4284286', // 密码
+  db_prefix: '', // 数据库表前缀
 };
