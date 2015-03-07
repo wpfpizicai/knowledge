5a149f2aedc677c0ef7a052aa77369662b2fbc8c
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-03-07 22:18:17 +0800

    add file by wangpengfei

--- App/Lib/Controller/Home/CourseController.js
+++ App/Lib/Controller/Home/CourseController.js
@@ -6,11 +6,12 @@ module.exports = Controller("Home/BaseController", function(){
   "use strict";
   return {
     indexAction: function(){
-      this.display({
+      this.assign({
         section : 'course',
         navLinks : navLinks,
         title : "课程"
       });
+      this.display();
     }
   };
 })
