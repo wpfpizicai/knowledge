39db964b8fb1207c928e3bb35d783d2c6846f9ce
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-03-07 13:20:48 +0800

    add files by wangpengfei

--- App/Lib/Controller/Home/IndexController.js
+++ App/Lib/Controller/Home/IndexController.js
@@ -4,6 +4,7 @@
  */
 module.exports = Controller("Home/BaseController", function(){
   "use strict";
+  var courses = [{name:"创造力提升",id:"creativedad",detail:"",source:"UNAM",time:"永久开放",img:"1.jpg"},{name:"跨领域医疗信息学",id:"creativedad",detail:"",source:"Minnesota",time:"永久开放",img:"2.png"},{name:"微观经济学",id:"creativedad",detail:"",source:"Illinois",time:"Dec 22nd",img:"3.jpg"},{name:"遗传学与进化概论",id:"creativedad",detail:"",source:"Duke",time:"Jan 1st",img:"4.jpg"}];
   return {
     indexAction: function(){
       //render View/Home/index_index.html file
@@ -11,7 +12,8 @@ module.exports = Controller("Home/BaseController", function(){
       this.assign({
         navLinks : navLinks,
         title : "首页",
-        users : users
+        users : users,
+        courses:courses
       })
       this.display();
     }
