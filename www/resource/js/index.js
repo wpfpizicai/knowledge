aeb39edc882e8aa0a850f947a68623b7ab9698aa
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-03-08 20:53:01 +0800

    add files by wangpengfei

--- www/resource/js/index.js
+++ www/resource/js/index.js
@@ -1,15 +1,8 @@
 define(function(require, exports, moudle) {
+  require("./widget/placeholder.js");
+  require("./widget/modal_btn.js");
+  require("./widget/validate.js");
   require('css/login.css');
-  var status = "";
-  $('#signin_btn').on('click',function(e){
-    status = 'signin';
-    draw(status);
-  });
-
-  $('#signup_btn').on('click',function(e){
-    status = "signup";
-    draw(status);
-  });
 
   function signupTpl(){
     /*
@@ -102,18 +95,265 @@ define(function(require, exports, moudle) {
     return m&&m[1]||m;
   };
 
-  function draw(m){
-    $(document.body).append(getTpl(m))
+  var m={
+    signin:function(){
+      var tpl=getTpl("signin");
+      return function(){
+        var m=$("#signin");
+        if(m.length) m.remove();
+        $("body").append(getTpl("signin"));
+        m=$("#signin");
+        m.on("shown",function(){
+          $(this).find("form").validateSetup({
+            fields:{
+              password:{
+                rules:[function(cb,v){
+                  if(!v){
+                    return "密码不能为空！";
+                  }
+                }],
+                keyup:function(e){
+                  if(e.keyCode=="13"){
+                    $("#signin-btn").trigger("click");
+                  }
+                  else{
+                    $(this).validate();
+                  }
+
+                }
+              }
+            },
+            onerror:function(e){
+              var $t,$d;
+              if(e._relateField&&e.tip){
+                $t=$(e._relateField);
+                $t.addClass("ipt-error").next(".rlf-tip-wrap").html(e.tip).addClass("rlf-tip-error");
+                ($d=$t.data("placeholder-textinput"))&&$d.addClass("ipt-error");
+              }
+              $("#signin-globle-error").removeClass("rlf-tip-error").empty();
+            },
+            onvalid:function(e){
+              var $t,$d;
+              if(e._relateField){
+                $t=$(e._relateField);
+                $t.removeClass("ipt-error").next(".rlf-tip-wrap").removeClass("rlf-tip-error").empty();
+                ($d=$t.data("placeholder-textinput"))&&$d.removeClass("ipt-error");
+              }
+              $("#signin-globle-error").removeClass("rlf-tip-error").empty();
+            }
+          });
+
+          $("#signin-btn").button({loadingText:"正在登录..."}).on("click",function(){
+            var $this=$(this);
+            if($this.hasClass("disabled")){ return;}
+            $this.button("loading");
+            $this.closest("form").validate({
+              success:function(vals){
+                var remember=$("#auto-signin")[0].checked?"1":"0";
+                $.ajax({
+                  url:"/Admin/login",
+                  data:{
+                    username:vals.email,
+                    password:vals.password,
+                    remember:remember
+                  },
+                  method:"post",
+                  dataType:"json",
+                  success:function(data){
+                    // if(data.status===1){
+                    //   fireLogined(data.data.userInfo);
+                    //   return ;
+                    // }
+                    // else if(data.status==5){
+                    //   window.location.href="/user/userfrozen";
+                    //   return ;
+                    // }
+                    // $("#signin-globle-error").addClass("rlf-tip-error").html(data.msg);
+                    //$("#signin-btn").button("reset");
+                  },
+                  error:function(){
+                    $("#signin-globle-error").addClass("rlf-tip-error").html("服务错误，稍后重试");
+                  },
+                  complete:function(){
+                    $("#signin-btn").button("reset");
+                  }
+
+                });
+
+              },
+              error:function(){
+                setTimeout('$("#signin-btn").button("reset");',1);
+              }
+            })
+          });
+          $(this).find("input").placeholder();
+        }).on("hidden",function(){
+          $(this).remove();
+        });
+        m.modal("show");
+      }
+    }(),
+    signup:function(){
+      var tpl=getTpl("signup");
+      return function(){
+        var m=$("#signup");
+        if(m.length) m.remove();
+        $("body").append(getTpl("signup"));
+        m=$("#signup");
+        m.on("shown",function(){
+          $(this).find("form").validateSetup({
+            fields:{
+              email:{
+                rules:[{
+                  rule:function(cb,v){
+                    return $.ajax({
+                      url:"/Admin/checkemail",
+                      method:"post",
+                      data:{email:v},
+                      dataType:"json",
+                      success:function(data){
+                        if(data.status==1){
+                          cb();
+                        }
+                        else{
+                          cb(data.msg)
+                        }
+                      }
+                    })
+                  }
+                }],
+                keyup:(function(){
+                  var interval=300,timer;
+                  return function(e){
+                    var $this=$(this);
+                    if(timer) clearTimeout(timer);
+                    timer=setTimeout(function(){
+                      $this.validate(e);
+                    },interval);
+                  }
+                })(),
+                blur:function(e){
+                  $(this).next(".rlf-tip-wrap").find("span").hide();
+                  $(this).validate(e);
+                },
+                focus:function(){
+                  $(this).next(".rlf-tip-wrap").find("span").show();
+                }
+              },
+              password:{
+                blur:function(e){
+                  $(this).next(".rlf-tip-wrap").find("span").hide();
+                  $(this).validate(e);
+                },
+                focus:function(){
+                  $(this).next(".rlf-tip-wrap").find("span").show();
+                },
+                onvalid: function(e) {
+                  $('input[name="cfmpwd"]', this.form).validate(e);
+                }
+              },
+              nick:{
+                blur:function(e){
+                  $(this).next(".rlf-tip-wrap").find("span").hide();
+                  $(this).validate(e);
+                },
+                focus:function(){
+                  $(this).next(".rlf-tip-wrap").find("span").show();
+                }
+              },
+              cfmpwd:{
+                rules:[function(cb,v) {
+                  if(!v) {
+                    return "验证密码不能为空！";
+                  }
+                  if($('input[name="password"]',this.form).val() !== v) {
+                    return '两次密码不一致！';
+                  }
+                }],
+                blur:function(e){
+                  $(this).next(".rlf-tip-wrap").find("span").hide();
+                  $(this).validate(e);
+                },
+                focus:function(){
+                  $(this).next(".rlf-tip-wrap").find("span").show();
+                }
+              }
+            },
+            onerror:function(e){
+              var $t,$d;
+              if(e._relateField&&e.tip){
+                $t=$(e._relateField);
+                $t.addClass("ipt-error").next(".rlf-tip-wrap").html(e.tip).addClass("rlf-tip-error");
+                ($d=$t.data("placeholder-textinput"))&&$d.addClass("ipt-error");
+              }
+            },
+            onvalid:function(e){
+              var $t,$d;
+              if(e._relateField){
+                $t=$(e._relateField);
+                $t.removeClass("ipt-error").next(".rlf-tip-wrap").removeClass("rlf-tip-error").empty();
+                ($d=$t.data("placeholder-textinput"))&&$d.removeClass("ipt-error");
+              }
+              $("#signup-globle-error").removeClass("rlf-tip-error").empty();
+            }
+          });
+
+          $(this).find("input").placeholder();
+
+          $("#signup-btn").button({loadingText:"正在注册..."})
+          .on("click",function(){
+            var $this=$(this);
+            if($this.hasClass("disabled")){ return;}
+            $this.button("loading");
+            $this.closest("form").validate({
+              success:function(vals){
+                $.ajax({
+                  url:"/Admin/register/",
+                  data:{
+                    username:vals.email,
+                    password:vals.password,
+                    nickname:vals.nick
+                  },
+                  method:"post",
+                  dataType:"json",
+                  success:function(data){
+                    if(data.status===0){
+                      fireLogined(data.data.userInfo,true);
+                      return ;
+                    }
+                    $("#signup-globle-error").addClass("rlf-tip-error").html(data.msg);
+
+                  },
+                  error:function(){
+                    $("#signup-globle-error").addClass("rlf-tip-error").html("服务错误，稍后重试");
+                  },
+                  complete:function(){
+                    $("#signup-btn").button("reset");
+                  }
+                });
+
+              },
+              error:function(){
+                setTimeout('$("#signup-btn").button("reset")',1);
+              }
+            })
+          });
+
+        }).on("hidden",function(){
+          $(this).remove();
+        });
+        m.modal("show");
+      }
+    }()
   };
 
-  function hide(){
+  $('#signin_btn').on('click',function(e){
+    m.signin();
+  });
 
-  };
-
-  function show(){
-
-  };
-
+  $('#signup_btn').on('click',function(e){
+    m.signup();
+  });
 })
 
 
