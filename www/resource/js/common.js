92a0b412abcd8571cdde8648d0d03874abcd9860
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-03-08 16:20:08 +0800

    add files by wangpengfei

---
+++ www/resource/js/common.js
@@ -0,0 +1,11 @@
+define(function(require, exports, moudle) {
+
+  $('#login_btn').on('click',function(e){
+    console.log(e)
+  });
+
+  $('#signin_btn').on('click',function(e){
+    console.log(e)
+  });
+
+})
