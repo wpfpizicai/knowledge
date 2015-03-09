237fcdc7ea13706ba95ec786ce8f59394f036d8d
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-03-09 23:12:41 +0800

    add files by wangpengfei

--- App/Lib/Controller/Home/UserController.js
+++ App/Lib/Controller/Home/UserController.js
@@ -6,32 +6,32 @@ module.exports = Controller("Home/BaseController", function(){
   "use strict";
   return {
     signinAction: function(){
-      // var self = this;
-      // //页面post
-      // if (self.isPost()) {
-      //     //用户登录成功写入Session
-      //     var email = self.post('email'); //获取post过来的用户名
-      //     var pwd = self.post('password'); //获取post过来的密码
-      //     console.log(email);
-      //     // return D('User').where({ //根据用户邮箱和密码查询符合条件的数据
-      //     //     email: email,
-      //     //     pwd: md5(pwd)
-      //     // }).find().then(function(data) {
-      //     //     console.log(data);
-      //     //     if (isEmpty(data)) {
-      //     //         //用户名或者密码不正确，返回错误信息
-      //     //         return self.error(403, '用户名或者密码不正确');
-      //     //     } else {
-      //     //         return self.session('userInfo', data);
-      //     //     }
-      //     // }).then(function() {
-      //     //     //登录成功跳转
-      //     //     return self.redirect('/');
-      //     // });
-      // } else {
-      //   //页面加载
-      //   return return self.redirect('/');
-      // }
+      var self = this;
+      //页面post
+      if (self.isPost()) {
+          //用户登录成功写入Session
+          var email = self.post('email'); //获取post过来的用户名
+          var pwd = self.post('password'); //获取post过来的密码
+
+          return D('user').where({ //根据用户邮箱和密码查询符合条件的数据
+              email: email,
+              pwd: md5(pwd)
+          }).find().then(function(data) {
+              console.log(data);
+              if (isEmpty(data)) {
+                  //用户名或者密码不正确，返回错误信息
+                  return self.error(403, '用户名或者密码不正确');
+              } else {
+                  return self.session('userInfo', data);
+              }
+          }).then(function() {
+              //登录成功跳转
+              return self.redirect('/');
+          });
+      } else {
+        //页面加载
+        return return self.redirect('/');
+      }
     },
 
     logoutAction: function() {
