a8753abc34168184851aa90cadf52a340d6167e0
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-03-09 23:06:01 +0800

    add files by wangpengfei

--- App/Lib/Controller/Home/BaseController.js
+++ App/Lib/Controller/Home/BaseController.js
@@ -20,7 +20,7 @@ module.exports = Controller(function(){
           if (self.isAjax()) {
               return self.error(403);
           }
-          self.userInfo = null;
+          self.userInfo = userInfo;
         } else {
           //用户已经登录
           self.userInfo = userInfo;
--- App/Lib/Controller/Home/UserController.js
+++ App/Lib/Controller/Home/UserController.js
@@ -4,18 +4,7 @@
  */
 module.exports = Controller("Home/BaseController", function(){
   "use strict";
-  var c_data = {
-    section : 'course',
-    navLinks : navLinks
-  };
   return {
-    indexAction: function(){
-      this.assign(extend({
-        title : "用户"
-      }, c_data));
-      this.display();
-    },
-
     signinAction: function(){
       var self = this;
       //页面post
@@ -23,26 +12,32 @@ module.exports = Controller("Home/BaseController", function(){
           //用户登录成功写入Session
           var email = self.post('email'); //获取post过来的用户名
           var pwd = self.post('password'); //获取post过来的密码
-
-          return D('User').where({ //根据用户邮箱和密码查询符合条件的数据
-              email: email,
-              pwd: md5(pwd)
-          }).find().then(function(data) {
-              console.log(data);
-              if (isEmpty(data)) {
-                  //用户名或者密码不正确，返回错误信息
-                  return self.error(403, '用户名或者密码不正确');
-              } else {
-                  return self.session('userInfo', data);
-              }
-          }).then(function() {
-              //登录成功跳转
-              return self.redirect('/');
-          });
+          console.log(email);
+          // return D('User').where({ //根据用户邮箱和密码查询符合条件的数据
+          //     email: email,
+          //     pwd: md5(pwd)
+          // }).find().then(function(data) {
+          //     console.log(data);
+          //     if (isEmpty(data)) {
+          //         //用户名或者密码不正确，返回错误信息
+          //         return self.error(403, '用户名或者密码不正确');
+          //     } else {
+          //         return self.session('userInfo', data);
+          //     }
+          // }).then(function() {
+          //     //登录成功跳转
+          //     return self.redirect('/');
+          // });
       } else {
-          //页面加载
-          return return self.redirect('/');
+        //页面加载
+        return return self.redirect('/');
       }
+    },
+
+    logoutAction: function() {
+      var self = this;
+      self.session('userInfo', '');
+      self.redirect('/');
     },
 
     signupAction: function(){
